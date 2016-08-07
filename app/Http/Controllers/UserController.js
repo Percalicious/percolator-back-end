3d3da51df7f820094fe5a6f6d7e5acfffc76d2c3
Niclas Willems <niclas.willems@gmail.com>
2016-08-07 15:32:28 -0400

    Updated user controller for login functionality.

--- app/Http/Controllers/UserController.js
+++ app/Http/Controllers/UserController.js
@@ -1,10 +1,43 @@
-* store (request, response) {
-  const User = use('App/Model/User'); // or import these above somewhere
-  const Hash = use('Hash');
-  const input = request.only('email', 'password');
-  input.password = yield Hash.make(input.password);
+'use strict';
 
-  const newUser = yield User.create(input);
+const User = use('App/Model/User');
+const Hash = use('Hash');
 
-  response.json(newUser.toJSON());
+class UserController {
+
+  * show (request, response) {
+    return response.json(request.authUser);
+  }
+
+  * store (request, response) {
+    // Takes user input & hashes the password
+    const input = request.only('email', 'password', 'firstName', 'lastName', 'mobileNumber', 'homeNumber', 'streetAddress', 'city', 'state', 'postCode', 'organization', 'category');
+    input.password = yield Hash.make(input.password);
+    // Create a new user
+    const user = yield User.create(input);
+    // Respond with updated user information in JSON object
+    return response.json(user.toJSON());
+  }
+
+  * login (request, response) {
+    // Receive input from the user
+    const input = request.only('email', 'password');
+
+    try {
+      // Find the user by email
+      const user = yield User.findBy('email', input.email);
+      // Verify their passwords matches & if not, let em know
+      const verify = yield Hash.verify(input.password, user.password);
+      if (!verify) { throw new Error('Password mismatch') };
+      // Generate a token
+      user.access_token = yield request.auth.generate(user);
+
+      return response.json(user.toJSON());
+    } catch (e) {
+      return response.status(401).json({ error: e.message });
+    }
+  }
+
 }
+
+module.exports = UserController;
