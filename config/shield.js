a4147daa21660c9c2076bba20815e634cb1a0d5e
Niclas Willems <niclas.willems@gmail.com>
2016-08-07 14:57:53 -0400

    Shield.js changes. Installed sqlite to project.

--- config/shield.js
+++ config/shield.js
@@ -81,10 +81,9 @@ module.exports = {
   |
   */
   csrf: {
-    enable: true,
+    enable: false,
     methods: ['POST', 'PUT', 'DELETE'],
-    filterUris: [],
-    compareHostAndOrigin: true
+    filterUris: []
   }
 
 }
