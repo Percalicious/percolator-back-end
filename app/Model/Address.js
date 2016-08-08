359e2ab7edd7518d52ed6a6227e1da41257a9273
Niclas Willems <niclas.willems@gmail.com>
2016-08-08 14:33:07 -0400

    Removed relationships from address model.

--- app/Model/Address.js
+++ app/Model/Address.js
@@ -4,17 +4,5 @@ const Lucid = use('Lucid')
 
 class Address extends Lucid {
 
-  user () {
-    return this.belongsTo('App/Model/User')
-  }
-
-  events () {
-    return this.belongsTo('App/Model/Event')
-  }
-
-  guests () {
-    return this.belongsTo('App/Model/Guest')
-  }
-
 }
 module.exports = Address
