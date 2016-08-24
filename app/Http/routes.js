06005e1dc619061c42cc0f41d17be0da5a68f253
Niclas Willems <niclas.willems@gmail.com>
2016-08-24 11:26:51 -0400

    Added route for getting user info for event creation page. Remove a bunch of unneeded commented out code.

--- app/Http/routes.js
+++ app/Http/routes.js
@@ -4,42 +4,36 @@ const Route = use('Route')
 
 Route.on('/').render('welcome')
 
+// Routes for User
 // User Registration & Login
 Route.post('/register', 'UserController.store')
 Route.post('/login', 'UserController.login')
 Route.get('/profile', 'UserController.show').middleware('auth')
 
 // Routes for Events
-
+// Grab user info to populate info on Create event form
+Route.get('/event-create', 'EventController.createInit').middleware('auth')
 // Create event for logged in/authorized user
-// Route.post('/event-create', 'EventController.store')
 Route.post('/event-create', 'EventController.store').middleware('auth')
 // All events for single user
 Route.get('/my-events', 'EventController.index').middleware('auth')
-
 // Single host view of event for a single user
 Route.get('host/my-events/:id', 'EventController.userSingleEvent').middleware('auth')
-// Route.get('host/my-events/:id', 'EventController.userSingleEvent')
-
 // Delete single event for a single user
 Route.delete('host/my-events/:id', 'EventController.destroy').middleware('auth')
-// Route.delete('host/my-events:id', 'EventController.destroy')
-
 // Send email to single invited guest
 Route.post('/', 'EventController.sendEmail')
-
 // Run WE report for single RSVPed guest
 Route.post('/runwe', 'EventController.runWEReport')
 
-//Create the EventGuest relationship
+// Routes for EventsGuests
+// Create the EventGuest relationship
 Route.post('/createEventGuest', 'EventGuestController.createEventGuest')
 Route.get('/event-guest/rsvp/:uuid', 'EventGuestController.guestEventView')
 Route.post('/event-guest/rsvp/:uuid', 'EventGuestController.guestEventUpdate')
 
 // Routes for Guests
 // Single host view of guest
-// Route.get('host/guests/:id', 'GuestController.index').middleware('auth')
-// Route.get('/host/guests/:id', 'GuestController.index')
 Route.post('/guests', 'GuestController.store').middleware('auth')
 Route.post('/guests/we-report', 'GuestController.saveWEReport')
 Route.get('/host/guests/:id', 'GuestController.fetchWEReport')
